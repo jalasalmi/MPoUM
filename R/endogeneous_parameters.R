001e87be6ac07c1e20c5997a6f8b165f10598b16
Jalasalmi <juho.alasalmi@ptt.fi>
2018-11-09 21:07:17 +0100

    Small fixes

--- R/endogeneous_parameters.R
+++ R/endogeneous_parameters.R
@@ -1,4 +1,4 @@
-r <- function(lambda, chi) {
+r <- function(lambda, chi,q) {
 
   output = q / (q + chi*(1-q)*(1-lambda))
   output
@@ -9,11 +9,11 @@ overlinelambda = 1 / (2*(1-q))
 sstarstar <- function(chi) {
 
   (delta * (t_high - t_low) * q) /
-    ((1 - t_low) * r(0,chi) - (1-overlinelambda)*(1-t_high)*r(overlinelambda,chi) - (t_high - t_low)*q)
+    ((1 - t_low) * r(0,chi,q) - (1-overlinelambda)*(1-t_high)*r(overlinelambda,chi,q) - (t_high - t_low)*q)
 }
 
 sstarstarstar <- function(chi) {
 
   (delta * (t_high - t_low) * (1-q)) /
-    ((t_high - t_low)*q + (1-t_high)*r(overlinelambda,chi) - (1-t_low)*r(0,chi))
+    ((t_high - t_low)*q + (1-t_high)*r(overlinelambda,chi,q) - (1-t_low)*r(0,chi,q))
 }
